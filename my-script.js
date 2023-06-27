dad78b21d42c5d59cfeb10ae41b30a8ad993c13b
Zukhra <zuhra.galieva.2014@gmail.com>
2023-06-27 12:31:34 +0100

    Move script from index file to my-script-js file

--- my-script.js
+++ my-script.js
@@ -91,5 +91,36 @@ function sortTable() {
     }
   }
 }
+
+function ResetForm() {
+  document.getElementById("movie").reset();
+}
+
+function addData() {
+  var table = document.getElementById("myTable"); // Get the table element in which you want to add row
+  var row = table.insertRow(1); // Create an empty <tr> element and add it to the 2nd position of the table: 
+  var cell1 = row.insertCell(0); // Insert new cells (<td> elements) at the 1st position :
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  var cell4 = row.insertCell(3);
+  var cell5 = row.insertCell(4);
+  var cell6 = row.insertCell(5);
+  // Add data to the new cells:
+  cell1.innerHTML = document.getElementById("title").value;
+  cell2.innerHTML = document.getElementById("plot").value;
+  cell3.innerHTML = document.getElementById("rating").value;
+  cell4.innerHTML = document.getElementById("year").value;
+  cell5.innerHTML = document.getElementById("cast").value;
+  cell6.innerHTML = document.getElementById("runtime").value;
+  movieData[document.getElementById("title").value] = {
+      rating: document.getElementById("rating").value,
+      runtime: Number(document.getElementById("runtime").value),
+      year: Number(document.getElementById("year").value),
+      plot: document.getElementById("plot").value,
+      cast: document.getElementById("cast").value.split(','),
+  };
+  console.log(movieData);
+  ResetForm();
+}
 //console.log(Object.values(movieData));
 
